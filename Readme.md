cbad535a77067fbabd3bcf9ce715a3d3f691d7ff
Andrey Asfaganov <aasfaganov@twilio.com>
2020-04-24 18:50:52 +0200

    init commit

---
+++ Readme.md
@@ -0,0 +1,21 @@
+
+# Simple scenarios running services in Docker
+
+
+
+
+# How to use it
+
+1. In your terminal navigate to the root directory of the repo
+
+2. Build the images using docker-compose:
+
+```
+docker-compose up
+```
+
+3. Navigate to localhost:8080 and check the results
+
+
+
+PS. don't forget `docker-compose up --build` if you want to rebuild the images after changing some code.
---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const redis = require('redis')
+const app = express()
+
+const client = redis.createClient({
+    host: 'redis-server',
+    port: 6379
+})
+
+client.set('visits', 0);
+
+app.get('/', (req, res) => {
+    client.get('visits', (err, visits) => {
+        let updated = parseInt(visits) +1;
+        res.send('Hello world ' + updated + " times")
+        client.set('visits', updated)
+    })
+
+})
+
+app.listen(8080, ()=>{
+    console.log('Listening on port 8080')
+})
